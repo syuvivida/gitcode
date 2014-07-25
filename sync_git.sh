3f430017ff7a4b8cadd31044a2277ea96024fe12
Henry <henryyy040606@hotmail.com>
2014-07-26 04:28:23 +0800

    sync_git

---
+++ sync_git.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -z $2 ]; then
+    echo "Usage: $0 [fragment] [comment]"
+    exit 1
+fi
+
+git add $1
+git commit -m "$2"
+git pull --rebase upstream master
+git push origin master
+
+exit
