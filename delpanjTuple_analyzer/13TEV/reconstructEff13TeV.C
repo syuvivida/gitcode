ddfe0f6d9c6b34f29944cf671faee31e1697a5e9
Henry <henryyy040606@hotmail.com>
2014-12-07 18:28:01 +0800

    xzh

--- delpanjTuple_analyzer/13TEV/reconstructEff13TeV.C
+++ delpanjTuple_analyzer/13TEV/reconstructEff13TeV.C
@@ -65,7 +65,7 @@ void reconstructEff13TeV(){
     Float_t* genParPhi   = data.GetPtrFloat("genParPhi");
     Float_t* genParM     = data.GetPtrFloat("genParM");
       
-    Int_t genMuIndex[2] = {-1,-1};
+    Int_t genMuIndex[2][2] = {{-1,-1},{-1,-1}};
 
     for(Int_t i = 0; i < nGenPar; i++){
 
@@ -75,31 +75,41 @@ void reconstructEff13TeV(){
       if( genParPt[i] <= 20 ) continue;
       if( fabs(genParEta[i]) >= 2.4 ) continue;
 
-      if( genMuIndex[0] < 0 ) genMuIndex[0] = i;
-      else if( genMuIndex[1] < 0 ) genMuIndex[1] = i;
+      if( genMuIndex[0][0] < 0 ) genMuIndex[0][0] = i;
+      else if( genMuIndex[0][1] < 0 ) genMuIndex[0][1] = i;
+      else if( genMuIndex[1][0] < 0 ) genMuIndex[1][0] = i; 
+      else if( genMuIndex[1][1] < 0 ) genMuIndex[1][1] = i;
 
-      if( genMuIndex[0] >= 0 && genMuIndex[1] >= 0 ) break;
+      if( (genMuIndex[0][0] >= 0 && genMuIndex[0][1] >= 0 ) ||
+	  (genMuIndex[0][0] >= 0 && genMuIndex[0][1] >= 0 &&
+	   genMuIndex[1][0] >= 0 && genMuIndex[1][1] >= 0 )) break;
 
     }
 
-    if( genMuIndex[0] < 0 || genMuIndex[1] < 0 ) continue;
+    if( genMuIndex[0][0] < 0 || 
+	genMuIndex[0][1] < 0 ) continue;
 
-    TLorentzVector genMuon[2];
+    TLorentzVector genMuon[2][2];
+
+    for(Int_t i = 0; i < 2; i++){
+      for(Int_t j = 0; j < 2; j++){
+
+	genMuon[i][j].SetPtEtaPhiM(genParPt[genMuIndex[i][j]], 
+				   genParEta[genMuIndex[i][j]], 
+				   genParPhi[genMuIndex[i][j]], 
+				   genParM[genMuIndex[i][j]]
+				   );
+      } 
+    }
+
+    Double_t gendR[2];
 
     for(Int_t i = 0; i < 2; i++){
 
-      genMuon[i].SetPtEtaPhiM(genParPt[genMuIndex[i]], 
-			      genParEta[genMuIndex[i]], 
-			      genParPhi[genMuIndex[i]], 
-			      genParM[genMuIndex[i]]
-			      );
-    
+      gendR[i] = genMuon[i][0].DeltaR(genMuon[i][1]);
+      h_DeltaR_denom->Fill(gendR[i]);
+
     }
-
-    Double_t gendR = genMuon[0].DeltaR(genMuon[1]);
-
-    h_DeltaR_denom->Fill(gendR);
-
 
     // Overloop all reconstructed muons that have possible matches
 
@@ -116,72 +126,80 @@ void reconstructEff13TeV(){
     Double_t dRMin = 0.1;
     Bool_t findPair[3] = {false, false, false}; //gg,gt,tt
 
-    for(Int_t i = 0; i < nMu; i++){ // i for loop
+    for(Int_t gm = 0; gm < 2; gm++){ // genmu for loop
 
-      if( muPt[i] <= 10 ) continue;
-      if( fabs(muEta[i]) >= 2.4 ) continue;
-      if( !isGlobalMuon[i] && !isTrackerMuon[i] )continue;
+      for(Int_t i = 0; i < nMu; i++){ // i for loop
+
+	if( muPt[i] <= 10 ) continue;
+	if( fabs(muEta[i]) >= 2.4 ) continue;
+	if( !isGlobalMuon[i] && !isTrackerMuon[i] )continue;
      
-      TLorentzVector stRecoMu(0,0,0,0);
+	TLorentzVector stRecoMu(0,0,0,0);
 
-      stRecoMu.SetPtEtaPhiM(muPt[i], 
-			    muEta[i], 
-			    muPhi[i], 
-			    muM[i]
-			    );
-      
-      Double_t dR_reco0gen0 = stRecoMu.DeltaR(genMuon[0]);
-      Double_t dR_reco0gen1 = stRecoMu.DeltaR(genMuon[1]);
-
-      if( dR_reco0gen0 > dRMin && dR_reco0gen1 > dRMin )continue;
-
- 
-      for(Int_t j = 0; j < i; j++){ // j for loop
-
-	if( muPt[j] <= 10 ) continue;
-	if( fabs(muEta[j]) >= 2.4 ) continue;
-	if( !isGlobalMuon[j] && !isTrackerMuon[j] )continue;
-     
-	TLorentzVector ndRecoMu(0,0,0,0);
-
-	ndRecoMu.SetPtEtaPhiM(muPt[j], 
-			      muEta[j], 
-			      muPhi[j], 
-			      muM[j]
+	stRecoMu.SetPtEtaPhiM(muPt[i], 
+			      muEta[i], 
+			      muPhi[i], 
+			      muM[i]
 			      );
       
-	Double_t dR_reco1gen0 = ndRecoMu.DeltaR(genMuon[0]);
-	Double_t dR_reco1gen1 = ndRecoMu.DeltaR(genMuon[1]);
+	Double_t dR_reco0gen0 = stRecoMu.DeltaR(genMuon[gm][0]);
+	Double_t dR_reco0gen1 = stRecoMu.DeltaR(genMuon[gm][1]);
 
-	if( dR_reco1gen0 > dRMin && dR_reco1gen1 > dRMin )continue;
+	if( dR_reco0gen0 > dRMin && dR_reco0gen1 > dRMin )continue;
 
-	// Pass 3 cases
+ 
+	for(Int_t j = 0; j < i; j++){ // j for loop
 
-	Bool_t glbglb = (isGlobalMuon[i] && isGlobalMuon[j]);
-	Bool_t atleastglb = (isGlobalMuon[i] && isGlobalMuon[j]) || (isGlobalMuon[i] && isTrackerMuon[j]) || (isGlobalMuon[j] && isTrackerMuon[i]);
-	Bool_t trktrk = (isTrackerMuon[i] && isTrackerMuon[j]);
-	Bool_t muMatching = ((dR_reco0gen0 < dRMin && dR_reco1gen1 < dRMin) || (dR_reco0gen1 < dRMin && dR_reco1gen0 < dRMin));
+	  if( muPt[j] <= 10 ) continue;
+	  if( fabs(muEta[j]) >= 2.4 ) continue;
+	  if( !isGlobalMuon[j] && !isTrackerMuon[j] )continue;
+     
+	  TLorentzVector ndRecoMu(0,0,0,0);
 
-	if( glbglb && muMatching ) 
-	  findPair[0] = true;
+	  ndRecoMu.SetPtEtaPhiM(muPt[j], 
+				muEta[j], 
+				muPhi[j], 
+				muM[j]
+				);
+      
+	  Double_t dR_reco1gen0 = ndRecoMu.DeltaR(genMuon[gm][0]);
+	  Double_t dR_reco1gen1 = ndRecoMu.DeltaR(genMuon[gm][1]);
 
-	if( atleastglb && muMatching )	   
-	  findPair[1] = true;
+	  if( dR_reco1gen0 > dRMin && dR_reco1gen1 > dRMin )continue;
 
-	if( trktrk && muMatching )
-	  findPair[2] = true;
+	  // Pass 3 cases
+
+	  Bool_t glbglb = (isGlobalMuon[i] && isGlobalMuon[j]);
+	  Bool_t atleastglb = (isGlobalMuon[i] && isGlobalMuon[j]) || (isGlobalMuon[i] && isTrackerMuon[j]) || (isGlobalMuon[j] && isTrackerMuon[i]);
+	  Bool_t trktrk = (isTrackerMuon[i] && isTrackerMuon[j]);
+	  Bool_t muMatching = ((dR_reco0gen0 < dRMin && dR_reco1gen1 < dRMin) || (dR_reco0gen1 < dRMin && dR_reco1gen0 < dRMin));
+
+	  if( glbglb && muMatching ) 
+	    findPair[0] = true;
+
+	  if( atleastglb && muMatching )	   
+	    findPair[1] = true;
+
+	  if( trktrk && muMatching )
+	    findPair[2] = true;
 	
 
-      } // end of j for loop
+	} // end of j for loop
 
-    } // end of i for loop
+      } // end of i for loop
+
+    } // end of genmu loop
 
 
     // Fill histogram with 3 cases
 
-    if(findPair[0]) h_DeltaR_glbglb->Fill(gendR);
-    if(findPair[1]) h_DeltaR_glbtrc->Fill(gendR);
-    if(findPair[2]) h_DeltaR_trctrc->Fill(gendR); 
+    for(Int_t i = 0; i < 2; i++){
+
+      if(findPair[0]) h_DeltaR_glbglb->Fill(gendR[i]);
+      if(findPair[1]) h_DeltaR_glbtrc->Fill(gendR[i]);
+      if(findPair[2]) h_DeltaR_trctrc->Fill(gendR[i]);
+
+    } 
 
 
   } // End of event loop
@@ -210,13 +228,13 @@ void reconstructEff13TeV(){
   c->Divide(2,2);
 
   c->cd(1);
-  h_reconstrcEff_glbglb->Draw();
+  h_reconstrcEff_glbglb->Draw("ap");
 
   c->cd(2);
-  h_reconstrcEff_glbtrc->Draw();
+  h_reconstrcEff_glbtrc->Draw("ap");
 
   c->cd(3);
-  h_reconstrcEff_trctrc->Draw();
+  h_reconstrcEff_trctrc->Draw("ap");
 
   c->Print("13TeVreconstructEff.png");
 
