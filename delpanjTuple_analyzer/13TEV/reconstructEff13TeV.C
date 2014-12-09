94830caafbc157455939231d0e044418057b8372
Henry <henryyy040606@hotmail.com>
2014-12-09 20:51:42 +0800

    xzh

--- delpanjTuple_analyzer/13TEV/reconstructEff13TeV.C
+++ delpanjTuple_analyzer/13TEV/reconstructEff13TeV.C
@@ -23,28 +23,16 @@ void reconstructEff13TeV(){
 
   TreeReader data("../../tree_RSGravToZZ_kMpl01_M-4500_Tune4C_13TeV-pythia8_Phys14DR.root");
 
-  // Declare the histogram
+  TGraphAsymmErrors* h_reconstrcEff[3]; // glbglb,glbtrc,trctrc
+  TH1D* h_DeltaRtemp = new TH1D("h_DeltaRtemp", "", 50, 0, 2);
+  TH1D* h_DeltaR[4]; // denom,glbglb,glbtrc,trctrc 
 
-  TH1D* h_deltaRtemplate = new TH1D("h_deltaRtemplate", "", 50, 0, 2);
+  for(Int_t i = 0; i < 4; i++){
 
-  TH1D* h_DeltaR_denom  = (TH1D*)h_deltaRtemplate->Clone("h_DeltaR_denom");
-  TH1D* h_DeltaR_glbglb = (TH1D*)h_deltaRtemplate->Clone("h_DeltaR_glbglb");
-  TH1D* h_DeltaR_glbtrc = (TH1D*)h_deltaRtemplate->Clone("h_DeltaR_glbtrc");
-  TH1D* h_DeltaR_trctrc = (TH1D*)h_deltaRtemplate->Clone("h_DeltaR_trctrc");
+    h_DeltaR[i] = (TH1D*)h_DeltaRtemp->Clone(Form("h_DeltaR%d",i));
 
-  TGraphAsymmErrors* h_efftemplate = new TGraphAsymmErrors();
-  h_efftemplate->SetMarkerColor(kBlue);
-  h_efftemplate->SetMarkerStyle(20);
-  h_efftemplate->SetMarkerSize(0.7);
-
-  TGraphAsymmErrors* h_reconstrcEff_glbglb = (TGraphAsymmErrors*)h_efftemplate->Clone("h_reconstrcEff_glbglb");
-  TGraphAsymmErrors* h_reconstrcEff_glbtrc = (TGraphAsymmErrors*)h_efftemplate->Clone("h_reconstrcEff_glbtrc");
-  TGraphAsymmErrors* h_reconstrcEff_trctrc = (TGraphAsymmErrors*)h_efftemplate->Clone("h_reconstrcEff_trctrc");
-
-  h_reconstrcEff_glbglb->SetTitle("Efficiency_glbglb");
-  h_reconstrcEff_glbtrc->SetTitle("Efficiency_glbtrc");
-  h_reconstrcEff_trctrc->SetTitle("Efficiency_trctrc");
-
+  }
+  
   // Begin of event loop
 
   for (Long64_t ev = 0; ev < data.GetEntriesFast(); ev++){
@@ -53,8 +41,7 @@ void reconstructEff13TeV(){
       fprintf(stderr, "Processing event %lli of %lli\n", ev + 1, data.GetEntriesFast());
     data.GetEntry(ev);
 
-
-    // To pass the generator muons
+    // To pass the generator muons ID
 
     Int_t    nGenPar     = data.GetInt("nGenPar"); 
     Int_t*   genParId    = data.GetPtrInt("genParId");
@@ -94,14 +81,15 @@ void reconstructEff13TeV(){
     for(Int_t i = 0; i < 2; i++){
       for(Int_t j = 0; j < 2; j++){
 
-	if( genMuIndex[i][j] < 0 )
-	  genMuon[i][j] = TLorentzVector(-999,-999,-999,-999);
+	if( genMuIndex[1][0] < 0 && genMuIndex[1][1] < 0 )
+	  break;
 
 	genMuon[i][j].SetPtEtaPhiM(genParPt[genMuIndex[i][j]], 
 				   genParEta[genMuIndex[i][j]], 
 				   genParPhi[genMuIndex[i][j]], 
 				   genParM[genMuIndex[i][j]]
 				   );
+
       } 
     }
 
@@ -109,8 +97,13 @@ void reconstructEff13TeV(){
 
     for(Int_t i = 0; i < 2; i++){
 
+
+      if( genMuIndex[1][0] < 0 && genMuIndex[1][1] < 0 )
+	break;
+
+
       gendR[i] = genMuon[i][0].DeltaR(genMuon[i][1]);
-      h_DeltaR_denom->Fill(gendR[i]);
+      h_DeltaR[0]->Fill(gendR[i]);
 
     }
 
@@ -131,6 +124,9 @@ void reconstructEff13TeV(){
 
     for(Int_t gm = 0; gm < 2; gm++){ // genmu for loop
 
+      if( genMuIndex[1][0] < 0 && genMuIndex[1][1] < 0 )
+	break;
+
       for(Int_t i = 0; i < nMu; i++){ // i for loop
 
 	if( muPt[i] <= 10 ) continue;
@@ -139,18 +135,13 @@ void reconstructEff13TeV(){
      
 	TLorentzVector stRecoMu(0,0,0,0);
 
-	stRecoMu.SetPtEtaPhiM(muPt[i], 
-			      muEta[i], 
-			      muPhi[i], 
-			      muM[i]
-			      );
+	stRecoMu.SetPtEtaPhiM(muPt[i], muEta[i], muPhi[i], muM[i]);
       
 	Double_t dR_reco0gen0 = stRecoMu.DeltaR(genMuon[gm][0]);
 	Double_t dR_reco0gen1 = stRecoMu.DeltaR(genMuon[gm][1]);
 
 	if( dR_reco0gen0 > dRMin && dR_reco0gen1 > dRMin )continue;
 
- 
 	for(Int_t j = 0; j < i; j++){ // j for loop
 
 	  if( muPt[j] <= 10 ) continue;
@@ -159,11 +150,7 @@ void reconstructEff13TeV(){
      
 	  TLorentzVector ndRecoMu(0,0,0,0);
 
-	  ndRecoMu.SetPtEtaPhiM(muPt[j], 
-				muEta[j], 
-				muPhi[j], 
-				muM[j]
-				);
+	  ndRecoMu.SetPtEtaPhiM(muPt[j], muEta[j], muPhi[j], muM[j]);
       
 	  Double_t dR_reco1gen0 = ndRecoMu.DeltaR(genMuon[gm][0]);
 	  Double_t dR_reco1gen1 = ndRecoMu.DeltaR(genMuon[gm][1]);
@@ -186,61 +173,48 @@ void reconstructEff13TeV(){
 	  if( trktrk && muMatching )
 	    findPair[gm][2] = true;
 	
-
 	} // end of j for loop
 
       } // end of i for loop
 
-    } // end of genmu loop
-    
+    } // end of genmu loop    
 
     // Fill histogram with 3 cases
-
-    for(Int_t gm = 0; gm < 2; gm++){
-      for(Int_t i = 0; i < 2; i++){
-
-	if(findPair[gm][0]) h_DeltaR_glbglb->Fill(gendR[i]);
-	if(findPair[gm][1]) h_DeltaR_glbtrc->Fill(gendR[i]);
-	if(findPair[gm][2]) h_DeltaR_trctrc->Fill(gendR[i]);
-
-      }
-    } 
-
     
+    for(Int_t gm = 0; gm < 2; gm++){
+      for(Int_t i = 0; i < 3; i++){
+
+	if(findPair[gm][i]) h_DeltaR[i+1]->Fill(gendR[gm]);
+
+      } 
+    }
+       
   } // End of event loop
 
   fprintf(stderr, "Processed all events\n");
 
-  h_reconstrcEff_glbglb->BayesDivide(h_DeltaR_glbglb, h_DeltaR_denom);
-  h_reconstrcEff_glbtrc->BayesDivide(h_DeltaR_glbtrc, h_DeltaR_denom);
-  h_reconstrcEff_trctrc->BayesDivide(h_DeltaR_trctrc, h_DeltaR_denom);
-
-  h_reconstrcEff_glbtrc->GetXaxis()->SetTitle("DeltaR");
-  h_reconstrcEff_glbglb->GetXaxis()->SetTitle("DeltaR");
-  h_reconstrcEff_trctrc->GetXaxis()->SetTitle("DeltaR");
-  h_reconstrcEff_glbtrc->GetYaxis()->SetTitle("Efficiency");
-  h_reconstrcEff_glbglb->GetYaxis()->SetTitle("Efficiency");
-  h_reconstrcEff_trctrc->GetYaxis()->SetTitle("Efficiency");
-
-  h_reconstrcEff_glbtrc->SetMinimum(0);
-  h_reconstrcEff_glbglb->SetMinimum(0);
-  h_reconstrcEff_trctrc->SetMinimum(0);
-  h_reconstrcEff_glbtrc->SetMaximum(1.2);
-  h_reconstrcEff_glbglb->SetMaximum(1.2);
-  h_reconstrcEff_trctrc->SetMaximum(1.2);
-
   TCanvas* c = new TCanvas("c", "", 0, 0, 1280, 720);
   c->Divide(2,2);
 
-  c->cd(1);
-  h_reconstrcEff_glbglb->Draw("ap");
+  std::string title[3]={"Two Global muons","Inclusive 1 Global + 1 Trk","Two Tracker muons"};
 
-  c->cd(2);
-  h_reconstrcEff_glbtrc->Draw("ap");
+  for(Int_t he = 0; he < 3; he++){
 
-  c->cd(3);
-  h_reconstrcEff_trctrc->Draw("ap");
+    c->cd(he+1);
+    h_reconstrcEff[he] = new TGraphAsymmErrors();
+    h_reconstrcEff[he]->SetTitle(title[he].data());
+    h_reconstrcEff[he]->SetMarkerColor(kBlue);
+    h_reconstrcEff[he]->SetMarkerStyle(20);
+    h_reconstrcEff[he]->SetMarkerSize(0.7);
+    h_reconstrcEff[he]->BayesDivide(h_DeltaR[he+1], h_DeltaR[0]);
+    h_reconstrcEff[he]->GetXaxis()->SetTitle("DeltaR");
+    h_reconstrcEff[he]->GetYaxis()->SetTitle("Efficiency");
+    h_reconstrcEff[he]->SetMinimum(0);
+    h_reconstrcEff[he]->SetMaximum(1.2);
+    h_reconstrcEff[he]->Draw("ap");
+
+  }
 
   c->Print("13TeVreconstructEff.png");
-
+  
 }
