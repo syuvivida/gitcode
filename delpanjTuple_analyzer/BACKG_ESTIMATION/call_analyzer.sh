a05202f025cf742f94f4145f2e645aaa80dca385
Henry <henryyy040606@hotmail.com>
2014-12-17 16:41:08 +0800

    xzh

--- delpanjTuple_analyzer/BACKG_ESTIMATION/call_analyzer.sh
+++ delpanjTuple_analyzer/BACKG_ESTIMATION/call_analyzer.sh
@@ -7,12 +7,13 @@ fi
 
 ##---------------- Run the analyzer macro ----------------##
 
-#DATAAA_DIR=$PWD/../../delpanjTuple_data/muon
-#BACKGR_DIR=$PWD/../../delpanjTuple_background/minorBkg
-BACKGR_DIR=$PWD/../../delpanjTuple_background/testBkg
+DATAAA_DIR=$PWD/../../delpanjTuple_data/totalmuon
+BACKGR_DIR=$PWD/../../delpanjTuple_background/DY
+#BACKGR_DIR=$PWD/../../delpanjTuple_background/testBkg
 
 ls -l $BACKGR_DIR | tail -n+2 | awk '{ print "echo -e \"\n\nProcessing root file --> "$9"\"; root -q -b -l '"$1"'.C++\\(\\\"'"$BACKGR_DIR"'/"$9"\\\",\\\""$9"\\\"\\); echo -e \"\nDone!\n\""}' | bash
 
+ls -l $DATAAA_DIR | tail -n+2 | awk '{ print "echo -e \"\n\nProcessing root file --> "$9"\"; root -q -b -l '"$1"'.C++\\(\\\"'"$DATAAA_DIR"'/"$9"\\\",\\\""$9"\\\"\\); echo -e \"\nDone!\n\""}' | bash
 
 rm -f *.d *.so
 echo -e "All jobs done!\n"
