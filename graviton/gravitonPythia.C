0bf315520d0701f6a1e7c83651dabc7d110f2c2b
Henry <henryyy040606@hotmail.com>
2015-04-25 00:21:10 +0800

    v4

--- graviton/gravitonPythia.C
+++ graviton/gravitonPythia.C
@@ -13,11 +13,11 @@ void gravitonPythia(){
   TreeReader data("pythia8_RS_WW.root");
 
   TH1F* h_cosTh = new TH1F("h_cosTh","RS Graviton by Pythia8",100,-1,1);
-  
-  h_cosTh->SetXTitle("cos#theta_1 in the W rest frame");
   h_cosTh->SetMinimum(0);
   h_cosTh->Sumw2();
 
+  TH1F *h_cosThStar = (TH1F*)h_cosTh->Clone("h_cosThStar");
+  
   for(Long64_t ev = 0 ; ev < data.GetEntriesFast(); ev++){
 
     data.GetEntry(ev);
@@ -29,6 +29,9 @@ void gravitonPythia(){
     Float_t* genParEta = data.GetPtrFloat("genParEta");
     Float_t* genParPhi = data.GetPtrFloat("genParPhi");
     Float_t* genParM = data.GetPtrFloat("genParM");
+
+
+    // cos#theta_1 in the W rest frame
       
     Int_t chgLepID = -1;
     Int_t neuLepID = -1;
@@ -67,12 +70,60 @@ void gravitonPythia(){
 
     }
 
+
+
+    // cos#theta* in the RSG rest frame
+
+    Int_t WplusID = -1;
+    Int_t WminusID = -1;
+
+    TLorentzVector Wplus(0,0,0,0);
+    TLorentzVector Wminus(0,0,0,0);
+
+    for(Int_t i = 0; i < nGenPar; i++){
+
+      if( genParSt[i] != 22 ) continue;
+      if( genParId[i] == +24 ) WplusID = i;
+      if( genParId[i] == -24 ) WminusID = i;
+
+      Wplus.SetPtEtaPhiM(genParPt[WplusID], 
+			 genParEta[WplusID], 
+			 genParPhi[WplusID], 
+			 genParM[WplusID]);
+
+      Wplus.SetPtEtaPhiM(genParPt[WminusID], 
+			 genParEta[WminusID], 
+			 genParPhi[WminusID], 
+			 genParM[WminusID]);
+
+      TLorentzVector RSG = Wplus + Wminus;
+
+      TVector3 RSGP = RSG.Vect();
+      TVector3 gv = -RSG.BoostVector();
+
+      Wplus.Boost(gv);
+
+      TVector3 WplusP = Wplus.Vect();
+
+      Double_t cosThStar = TMath::Cos(WplusP.Angle(RSGP));
+
+      h_cosThStar->Fill(cosThStar);
+
+    }
+
+
   }
 
-  TCanvas* c = new TCanvas("c","",0,0,1024,768);
-  c->cd();
+  TCanvas* c = new TCanvas("c","",0,0,1000,600);
+  c->Divide(2,1);
 
+  c->cd(1); 
+  h_cosTh->SetXTitle("cos#theta_1 in the W rest frame");
   h_cosTh->Draw();
+
+  c->cd(2); 
+  h_cosThStar->SetXTitle("cos#theta* in the RSG rest frame");
+  h_cosThStar->Draw();
 
   c->Print("RSgravitonPythia.pdf");
     
