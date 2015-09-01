afebe34d2bba5a15cb7f864a05d42e980682e90d
Henry <henryyy040606@hotmail.com>
2015-09-01 20:55:42 +0800

    13tev

--- electron_ID_comparison/eleVariable.C
+++ electron_ID_comparison/eleVariable.C
@@ -49,6 +49,7 @@ void eleVariable(std::string inputFile, int num){
   TH1D* h_eleMissHits[2]; 
   TH1D* h_eleD0[2];   
   TH1D* h_eleMiniIso[2]; 
+  TH1D* h_dilepMass[2];
 
   for(Int_t i = 0; i < 2; i++){
     
@@ -61,6 +62,7 @@ void eleVariable(std::string inputFile, int num){
     h_eleMissHits[i]             = new TH1D(Form("h_eleMissHits%d",i), "eleMissHits", 100, 0, 1.5);
     h_eleD0[i]                   = new TH1D(Form("h_eleD0%d",i), "eleD0", 100, -0.015, 0.015);  
     h_eleMiniIso[i]              = new TH1D(Form("h_eleMiniIso%d",i), "eleMiniIso", 100, 0, 0.05);
+    h_dilepMass[i]               = new TH1D(Form("h_dilepMass%d",i), "dilepMass", 100, 50, 150);
 
     h_eleEtaseedAtVtx[i]        ->GetXaxis()->SetTitle("eleEtaseedAtVtx"); 
     h_eledPhiAtVtx[i]           ->GetXaxis()->SetTitle("eledPhiAtVtx");   
@@ -71,10 +73,13 @@ void eleVariable(std::string inputFile, int num){
     h_eleMissHits[i]            ->GetXaxis()->SetTitle("eleMissHits"); 
     h_eleD0[i]                  ->GetXaxis()->SetTitle("eleD0");      
     h_eleMiniIso[i]             ->GetXaxis()->SetTitle("eleMiniIso"); 
+    h_dilepMass[i]              ->GetXaxis()->SetTitle("dilepMass");
 
   }
 
   // begin of event loop
+
+  Int_t nPass = 0;
 
   for (Long64_t ev = 0; ev < data.GetEntriesFast(); ev++){
 
@@ -114,31 +119,38 @@ void eleVariable(std::string inputFile, int num){
       }
     }
 
-    // choosing the primary electron
+    if( !passTrigger ) continue;
+
+    // choosing electron
 
     Int_t eleId[2] = {-1,-1};
+    Float_t mll = -1;
     
     for(Int_t ie = 0; ie < nEle; ie++){
 
-      if( eleScEt[ie] <= 35 ) continue;
-      if( !eleEcalDrivenSeed[ie] ) continue; 
-      
+      if( !(fabs(eleScEta[ie]) < 1.442 || fabs(eleScEta[ie]) > 1.566) ) continue;
+      if( fabs(eleScEta[ie]) > 2.5 ) continue;
+
       TLorentzVector* thisEle = (TLorentzVector*)eleP4->At(ie);
       
       for(Int_t je = 0; je < ie; je++){
 
+	if( eleScEt[ie] <= 35 || eleScEt[je] <= 35 ) continue;
+	if( !eleEcalDrivenSeed[ie] || !eleEcalDrivenSeed[je] ) continue;
+	if( !(fabs(eleScEta[je]) < 1.442 || fabs(eleScEta[je]) > 1.566) ) continue;
+	if( fabs(eleScEta[je]) > 2.5 ) continue;
+
 	TLorentzVector* thatEle = (TLorentzVector*)eleP4->At(je);
 	
-	Float_t mll = (*thisEle+*thatEle).M();
+	mll = (*thisEle+*thatEle).M();
+	
 	Float_t pt1 = thisEle->Pt();
 	Float_t pt2 = thatEle->Pt();
 	Float_t ptmax = TMath::Max(pt1,pt2);
-	Float_t ptmin = TMath::Min(pt1,pt2);
 	
 	if( mll < 60 || mll > 120 ) continue;
 	if( ptmax < 115 ) continue;
-	if( ptmin < 20 ) continue;
-
+	
 	eleId[0] = ie;
 	eleId[1] = je;
 	break;
@@ -148,6 +160,8 @@ void eleVariable(std::string inputFile, int num){
 
     if(	eleId[0] < 0 || eleId[1] < 0 ) continue;
     
+    nPass++;
+
     // electron selections and cuts
 
     for(Int_t ie = 0; ie < 2; ie++){
@@ -161,12 +175,11 @@ void eleVariable(std::string inputFile, int num){
 	  if( fabs(eleEtaseedAtVtx[eleId[ie]])   >= 0.004     && flag != 0 ) continue;
 	  if( fabs(eledPhiAtVtx[eleId[ie]])      >= 0.06      && flag != 1 ) continue;
 	  if( eleHoverE[eleId[ie]]               >= 1/E+0.05  && flag != 2 ) continue;
-	  if( eleSigmaIEtaIEtaFull5x5[eleId[ie]] >= 999       && flag != 3 ) continue; 
-	  if( eleMissHits[eleId[ie]]             >  1         && flag != 4 ) continue;
-	  if( fabs(eleD0[eleId[ie]])             >= 0.02      && flag != 5 ) continue;
-	  if( eleMiniIso[eleId[ie]]              >= 0.1       && flag != 6 ) continue;
+	  if( eleMissHits[eleId[ie]]             >  1         && flag != 3 ) continue;
+	  if( fabs(eleD0[eleId[ie]])             >= 0.02      && flag != 4 ) continue;
+	  if( eleMiniIso[eleId[ie]]              >= 0.1       && flag != 5 ) continue;
 	  if( (eleE2x5Full5x5[eleId[ie]]/eleE5x5Full5x5[eleId[ie]] <= 0.94  &&
-	       eleE1x5Full5x5[eleId[ie]]/eleE5x5Full5x5[eleId[ie]] <= 0.83) && flag != 7 )
+	       eleE1x5Full5x5[eleId[ie]]/eleE5x5Full5x5[eleId[ie]] <= 0.83) && flag != 8 )
 	    continue;
 	  	    
 	  switch(flag){
@@ -182,6 +195,7 @@ void eleVariable(std::string inputFile, int num){
 	    h_eleFull5x5E2x5dvE5x5[0] ->Fill(eleE2x5Full5x5[eleId[ie]]/eleE5x5Full5x5[eleId[ie]]);
 	    h_eleFull5x5E1x5dvE5x5[0] ->Fill(eleE1x5Full5x5[eleId[ie]]/eleE5x5Full5x5[eleId[ie]]);
 	  } break;
+	  case 8: h_dilepMass[0]      ->Fill(mll);                        break;
 	    
 	  } // end of switch
     
@@ -201,10 +215,7 @@ void eleVariable(std::string inputFile, int num){
 	  if( eleMissHits[eleId[ie]]             >  1        && flag != 4 ) continue;
 	  if( fabs(eleD0[eleId[ie]])             >= 0.05     && flag != 5 ) continue;
 	  if( eleMiniIso[eleId[ie]]              >= 0.1      && flag != 6 ) continue;
-	  if( (eleE2x5Full5x5[eleId[ie]]/eleE5x5Full5x5[eleId[ie]] <= -999  &&
-	       eleE1x5Full5x5[eleId[ie]]/eleE5x5Full5x5[eleId[ie]] <= -999) && flag != 7 )
-	    continue;
-	  	    
+	  	  	    
 	  switch(flag){
 
 	  case 0: h_eleEtaseedAtVtx[1]->Fill(eleEtaseedAtVtx[eleId[ie]]); break;
@@ -218,6 +229,7 @@ void eleVariable(std::string inputFile, int num){
 	    h_eleFull5x5E2x5dvE5x5[1] ->Fill(eleE2x5Full5x5[eleId[ie]]/eleE5x5Full5x5[eleId[ie]]);
 	    h_eleFull5x5E1x5dvE5x5[1] ->Fill(eleE1x5Full5x5[eleId[ie]]/eleE5x5Full5x5[eleId[ie]]);
 	  } break;
+	  case 8: h_dilepMass[1]      ->Fill(mll);                        break;
 		    
 	  } // end of switch
 	
@@ -231,11 +243,13 @@ void eleVariable(std::string inputFile, int num){
 
   fprintf(stderr, "Processed all events\n");
   
+  std::cout << "pass events: " << nPass << std::endl;
+
   TFile* outFile[2];
   
-  std::string h_name[9] = {"eleEtaseedAtVtx","eledPhiAtVtx","eleHoverE","eleSigmaIEtaIEtaFull5x5",
+  std::string h_name[10] = {"eleEtaseedAtVtx","eledPhiAtVtx","eleHoverE","eleSigmaIEtaIEtaFull5x5",
 			   "eleFull5x5E2x5dvE5x5","eleFull5x5E1x5dvE5x5","eleMissHits","eleD0",
-			   "eleMiniIso"};
+			   "eleMiniIso","dilepMass"};
 
   std::string region[2] = {"barrel","endcap"};
 
@@ -252,7 +266,8 @@ void eleVariable(std::string inputFile, int num){
     h_eleMissHits[i]            ->Write(h_name[6].data());   
     h_eleD0[i]                  ->Write(h_name[7].data());      
     h_eleMiniIso[i]             ->Write(h_name[8].data());
-    
+    h_dilepMass[i]              ->Write(h_name[9].data());    
+
     outFile[i]->Write();
 
   }
